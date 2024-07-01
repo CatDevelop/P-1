893a85510e2f3fe60bf83d8ddfef6bab634dd4a0
m.rozhkov <maks.rozhkov.2004@gmail.com>
2024-07-01 16:42:10 +0500

    fix

--- frontend/src/components/Schemes/SchemeEditor/SchemeName/SchemeName.jsx
+++ frontend/src/components/Schemes/SchemeEditor/SchemeName/SchemeName.jsx
@@ -1,17 +1,11 @@
 import React, {useCallback} from "react"
 import styles from "./SchemeName.module.css"
 import classNames from "classnames";
-import s from "../../../TimeTrackingRow/TimeTrackingRow.module.css";
-import {Typography} from "antd";
-import clsx from "clsx";
+
 export const SchemeName = (props) => {
-    const handleChangeName = useCallback(() => {
-
-    }, [])
-
     return (
         <div className={styles.schemeName}>
-            <p className={clsx(styles.schemeName__title, !props.title && styles.schemeName__title_undefined)}>
+            <p className={classNames(styles.schemeName__title, !props.title && styles.schemeName__title_undefined)}>
                 {props.title || "Без названия"}
             </p>
             <p className={styles.schemeName__lastChange}>
